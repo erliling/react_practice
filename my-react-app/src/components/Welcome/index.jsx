e6c65ef6336679a4e105ea6bdde335b20cc8e414
erliling <erliling2022@gmail.com>
2024-12-13 17:03:12 -0600

    modified:   my-react-app/src/components/Welcome/index.jsx 	new file:   my-react-app/test/bind.html 	new file:   my-react-app/test/bind_event.html 	new file:   my-react-app/test/this.html 	new file:   my-react-app/test/usestrict.html

--- my-react-app/src/components/Welcome/index.jsx
+++ my-react-app/src/components/Welcome/index.jsx
@@ -2,15 +2,20 @@ import React, {useState} from 'react'
 
 export default function index() {
     const [isHot, setCount] = useState("hot") 
+
+    const demo = () => {
+        console.log("I've been clicked");
+        console.log("this = "+ this);
+      }
   return (
     <div>
         <h1 onClick={demo}>{isHot}</h1>
     </div>
   )
 
-  function demo() {
-    console.log("I've been clicked");
-    console.log("this = "+ this);
+//   function demo() {
+//     console.log("I've been clicked");
+//     console.log("this = "+ this);
 
-  }
+//   }
 }
