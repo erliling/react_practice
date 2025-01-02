3d89b39d59b4bd37158903a825861e6ab10e118b
erliling <erliling2022@gmail.com>
2025-01-02 16:37:05 -0600

    modified:   my-react-app/src/App.css 	modified:   my-react-app/src/App.jsx 	new file:   my-react-app/src/App_useeffect.jsx 	new file:   my-react-app/src/components/Footer/index.css 	new file:   my-react-app/src/components/Footer/index.jsx 	new file:   my-react-app/src/components/Header/index.css 	new file:   my-react-app/src/components/Header/index.jsx 	new file:   my-react-app/src/components/Item/index.css 	new file:   my-react-app/src/components/Item/index.jsx 	new file:   my-react-app/src/components/List/index.css 	new file:   my-react-app/src/components/List/index.jsx

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -5,42 +5,24 @@ import './App.css'
 import Hello from './components/Hello'
 import Welcome from './components/Welcome'
 import './components/Welcome/index.css'
+import Header from './components/Header'
+import List from './components/List'
+import Footer from './components/Footer'
+
 
 function App() {
   const [count, setCount] = useState(0)
   const prop = {name: "jerry", age: 20, gender:"male"}
 
   return (
-    <>
-    <div>
-      <Hello></Hello>
-    </div>
-    <div>
-      {/* <Welcome {...prop}></Welcome>
-      <Welcome name = "tom" age={12} gender="female"></Welcome> */}
-      <Welcome></Welcome>
-    </div>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='todo-contianer'>
+      <div className='todo-wrap'>
+        <Header></Header>
+        <List></List>
+        <Footer></Footer>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
+  
   )
 }
 
---
+++ my-react-app/src/components/List/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Item from '../Item'
+import './index.css'
+
+export default function index() {
+  return (
+    <ul className="todo-main">
+        <Item></Item>
+        <Item></Item>
+        <Item></Item>
+        <Item></Item>
+        <Item></Item>
+        <Item></Item>
+        <Item></Item>
+    </ul>
+  )
+}
