392c141c602cc2d6f024a6310c6b0eeb424ba4be
erliling <erliling2022@gmail.com>
2025-01-29 19:13:52 -0600

    modified:   my-react-app/src/App.jsx 	modified:   my-react-app/src/components/A copy/index.jsx 	modified:   my-react-app/src/components/A/index.jsx 	deleted:    my-react-app/src/components/B copy/index.jsx 	deleted:    my-react-app/src/components/C copy/index.jsx 	new file:   my-react-app/src/components/parenting_00/A copy/index.jsx 	new file:   my-react-app/src/components/parenting_00/App copy.jsx

--- my-react-app/src/components/A/index.jsx
+++ my-react-app/src/components/A/index.jsx
@@ -3,7 +3,7 @@ import B from '../B'
 import {myContext} from '../../context'
 
 
-export default function index() {
+export default function index(props) {
     const [name, setName] = useState('tom')
     
 
@@ -12,7 +12,7 @@ export default function index() {
         <h3>I'm  component A</h3>
         <h4>my username is: {name}</h4>
         <myContext.Provider value={{name, }}>
-            <B/>
+            {props.children}
         </myContext.Provider>
     </div>
   )
--- my-react-app/src/components/parenting_00/App copy.jsx
+++ my-react-app/src/components/parenting_00/App copy.jsx
@@ -1,10 +1,10 @@
 import {createTheme, ThemeProvider, alpha, getContrastRatio} from '@mui/material/styles'
 
-import Count from './containers/Count'
-import Person from './containers/Person'
-import store from './redux/store'
-import A from './components/A'
-import B from './components/B'
+import Count from '../../containers/Count'
+import Person from '../../containers/Person'
+import store from '../../redux/store'
+import A from '../A'
+import B from '../B'
 
 
 import './App.css'
